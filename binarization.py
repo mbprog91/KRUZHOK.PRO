62428ae33c693066efe55546ec98e0cc9cb530e6
Марк Бушмакин <57404307+mbprog91@users.noreply.github.com>
2020-10-04 10:40:57 +0300

    Add files via upload

--- binarization.py
+++ binarization.py
@@ -8,24 +8,25 @@ def resize_image(img):
     height = int(img.shape[0] * scale_percent / 100)
     output = cv2.resize(img, (width, height))
     return output
-smth, thresh1 = cv2.threshold(img_process, 120, 255, cv2.THRESH_BINARY)
-smth, thresh2 = cv2.threshold(img_process, 120, 255, cv2.THRESH_BINARY_INV)
-smth, thresh3 = cv2.threshold(img_process, 120, 255, cv2.THRESH_TRUNC)
-smth, thresh4 = cv2.threshold(img_process, 120, 255, cv2.THRESH_TOZERO)
-smth, thresh5 = cv2.threshold(img_process, 120, 255, cv2.THRESH_TOZERO_INV)
-th2 = cv2.adaptiveThreshold(img_process, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY,11,2)
-th3 = cv2.adaptiveThreshold(img_process, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,2)
-smth, thotsu = cv2.threshold(img_process, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-gauss_process = cv2.GaussianBlur(img_process, (5, 5), 0)
-smth, thotsu2 = cv2.threshold(gauss_process, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-cv2.imshow('1', resize_image(thresh1))
-cv2.imshow('2', resize_image(thresh2))
-cv2.imshow('3', resize_image(thresh3))
-cv2.imshow('4', resize_image(thresh4))
-cv2.imshow('5', resize_image(thresh5))
-cv2.imshow('6', resize_image(th2))
-cv2.imshow('7', resize_image(th3))
-cv2.imshow('8', resize_image(thotsu))
-cv2.imshow('9', resize_image(thotsu2))
-if cv2.waitKey(0) & 0xff == 27: 
-    cv2.destroyAllWindows() 
+def binar_image(img_process):
+    smth, thresh1 = cv2.threshold(img_process, 120, 255, cv2.THRESH_BINARY)
+    smth, thresh2 = cv2.threshold(img_process, 120, 255, cv2.THRESH_BINARY_INV)
+    smth, thresh3 = cv2.threshold(img_process, 120, 255, cv2.THRESH_TRUNC)
+    smth, thresh4 = cv2.threshold(img_process, 120, 255, cv2.THRESH_TOZERO)
+    smth, thresh5 = cv2.threshold(img_process, 120, 255, cv2.THRESH_TOZERO_INV)
+    th2 = cv2.adaptiveThreshold(img_process, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY,11,2)
+    th3 = cv2.adaptiveThreshold(img_process, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,2)
+    smth, thotsu = cv2.threshold(img_process, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    gauss_process = cv2.GaussianBlur(img_process, (5, 5), 0)
+    smth, thotsu2 = cv2.threshold(gauss_process, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    return {
+        'THRESH_BINARY': resize_image(thresh1),
+        'THRESH_BINARY_INV': resize_image(thresh2),
+        'THRESH_TRUNC': resize_image(thresh3),
+        'THRESH_TOZERO': resize_image(thresh4),
+        'THRESH_TOZERO_INV': resize_image(thresh5),
+        'ADAPTIVE_THRESH_MEAN_C': resize_image(th2),
+        'ADAPTIVE_THRESH_GAUSSIAN_C': resize_image(th3),
+        'THRESH_OTSU': resize_image(thotsu),
+        'THRESH_OTSU GAUSS': resize_image(thotsu2)
+        } 
